f7a4a7712d8dae6835f60f021ad04340b97d0710
Eva <yi.zheng1709@gmail.com>
2021-02-19 21:52:03 -0500

    adds playgame.erb to views directory; embeds repl link of CLI project

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ class Application < Sinatra::Base
     get '/' do
         erb :index
     end
+
+    #adds journal entry
+    get '/entry' do
+        erb :entry 
+    end
+
+    #adds checklist of organizer 
+    get '/organizer' do 
+        erb :organizer 
+    end
+
+    #add rubytopia; get '/playgame'??
 end
