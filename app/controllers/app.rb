8a28d7aad2fecc1fe208171e965bb89c04e07b7f
Eva <yi.zheng1709@gmail.com>
2021-02-25 00:53:43 -0500

    adds folders to separate the views; creates form for new organizer

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -24,11 +24,11 @@ class Application < Sinatra::Base
     end
 
     get '/sign-up' do
-        erb :new_user #shows form for signing up
+        erb ":/new-forms/new_user" #shows form for signing up
     end
 
     post '/signup' do #uses data from new_user.erb
-        @user = User.create(username: params[:username], password: params[:password])
+        @user = User.create(username: params[:username], password_digest: params[:password])
         #need to create session hash
         session[:user_id] = @user.id
         redirect to "user/entries" 
@@ -40,14 +40,21 @@ class Application < Sinatra::Base
         id = session[:user_id] 
         @entries = Entry.where('user.id == #{id}').order(:date)
         #how to specify the id that I want to look for?
-        erb ":users/entries" 
+        erb :"users/entries" 
     end
  
+    get '/organizers/new' do
+        erb :"new-forms/new_organizer"
+    end
 
+    post '/organizers' do
+        @organizer = Organizer.create(date_of_tasks: params[:date_of_tasks], tasks: params[:tasks])
+        redirect to "organizers/#{@organizer.id}"
+    end
     #show all organizers
     #add button to lead to adding new organizer
-    get '/organizer' do 
-        erb ":user/organizer" 
+    get '/organizers' do 
+        erb :"user/organizers"
     end
 
     #add rubytopia; get '/playgame'??
