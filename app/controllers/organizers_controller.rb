c0c8cdd6742e7e2a75d3a39b12cb3a11404d1299
Eva <yi.zheng1709@gmail.com>
2021-02-26 01:30:22 -0500

    in post /organizers route, writes an algorithm that deletes task_name ==  and then create a new task with the remaining tasks

--- app/controllers/organizers_controller.rb
+++ app/controllers/organizers_controller.rb
@@ -1,11 +1,14 @@
 class OrganizersController < ApplicationController
-    
+
     get '/organizers/new' do #doesnt load page properly
         erb :"/new_forms/new_organizer"
     end
 
     post '/organizers' do
-        @organizer = Organizer.create(date_of_tasks: params[:date_of_tasks], tasks: params[:tasks])
+        # @organizer = Organizer.create(date_of_tasks: params[:date_of_tasks], tasks: params[:tasks])
+        @organizer = Organizer.create(date_of_tasks: params[:date_of_tasks])
+        task_array = params[:tasks].delete_if {|task_name| task_name == "" }
+        task_array = task_array.collect{|task_name| Task.create(task_name) }
         redirect to "organizers/#{@organizer.id}"
     end
     #show all organizers
