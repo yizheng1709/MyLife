8d44b785bc0ccbbdd94fa1023a26d8351d102940
Eva <yi.zheng1709@gmail.com>
2021-02-28 16:57:14 -0500

    changes the input tag to textarea tag in entries/edit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -31,4 +31,40 @@ class EntriesController < ApplicationController
         @entries = current_user.entries.order("date DESC")
         erb :"/entries/entries"
     end
+
+    get '/entries/:id/edit' do
+        redirect_if_not_logged_in
+        @entry = find_entry
+        # binding.pry
+        if @entry.user_id == current_user.id
+            # binding.pry
+            erb :"/entries/edit"
+        else 
+            redirect "/entries/#{@entry.id}"
+        end 
+    end
+
+    patch '/entries/:id/edit' do
+        redirect_if_not_logged_in
+        #id will come from the form's name attribute
+        @entry = find_entry
+        params[:tasks][0].each do |id, taskname|
+            # binding.pry
+            task = Task.find_by(id: id)
+            task.update(task_name: taskname)
+        end
+        redirect to "/entries/#{@entry.id}"
+    end
+
+    delete '/organizers/:id/delete' do
+        redirect_if_not_logged_in
+        @organizer = find_organizer
+        if @organizer.user_id == current_user.id
+            @organizer.destroy
+            redirect to "/organizers"
+        else
+            redirect "/organizers"
+        end
+
+    end
 end
